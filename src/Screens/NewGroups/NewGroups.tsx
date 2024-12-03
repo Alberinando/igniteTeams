4ef040a75b1088bd47121172d91f2786f2a0b609
Albernando <alberinandocipriano@hotmail.com>
2024-12-03 14:15:02 -0300

    Feat: Error handling

--- src/Screens/NewGroups/NewGroups.tsx
+++ src/Screens/NewGroups/NewGroups.tsx
@@ -6,18 +6,31 @@ import {Button} from '@components/Button/Button';
 import {Input} from '@components/Input/Input';
 import {useNavigation} from '@react-navigation/native';
 import {groupCreate} from '../../Storange/group/groupCreate';
+import {AppError} from "@utils/AppError.ts";
+import {ToastAndroid} from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
   const navigation = useNavigation();
+
   async function handleNew() {
     try {
+      if(group.trim().length === 0){
+        return ToastAndroid.show('Informe o nome do grupo!', ToastAndroid.SHORT);
+      }
+
       await groupCreate(group);
       navigation.navigate('players', {group});
     } catch (error) {
-      console.error(error);
+      if(error instanceof AppError){
+        ToastAndroid.show(error.message, ToastAndroid.SHORT);
+      } else {
+        ToastAndroid.show('Não foi possível criar um novo grupo.', ToastAndroid.SHORT);
+        console.error(error);
+      }
     }
   }
+
   return (
     <Container>
       <Header showBackButton />
