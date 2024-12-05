7e26b97e87e9a37823d62bf3e020558de38b3b1a
Albernando <alberinandocipriano@hotmail.com>
2024-12-04 21:37:53 -0300

    Bug: CI/CD

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  testEnvironment: 'node',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-native-community))',
+  ],
 };
